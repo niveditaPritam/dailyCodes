53b03fb1532cd48d18ea6f785cbf18ed52297cac
Nivedita Pritam <166897201+niveditaPritam@users.noreply.github.com>
2025-03-28 12:53:15 +0530

    Create FirstMissingPositive.cpp

---
+++ FirstMissingPositive.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] <= 0 || nums[i] > n) {
+                nums[i] = n + 1;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            int num = abs(nums[i]);
+            if (num > n) {
+                continue;
+            }
+            num--; 
+            if (nums[num] > 0) {
+                nums[num] = -abs(nums[num]);
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > 0) {
+                return i + 1;
+            }
+        }
+
+        return n + 1;
+    }
+};
