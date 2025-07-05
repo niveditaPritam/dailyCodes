4c8fd22691f5da53929e810a09935b0139602bec
Nivedita Pritam <166897201+niveditaPritam@users.noreply.github.com>
2025-07-05 14:15:02 +0530

    Find Lucky Integer in an Array.java

---
+++ Find Lucky Integer in an Array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findLucky(int[] arr) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int num : arr) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        int max = -1;
+
+        for (int key : map.keySet()) {
+            if (key == map.get(key)) {
+                max = Math.max(max, key);
+            }
+        }
+
+        return max;
+    }
+}
