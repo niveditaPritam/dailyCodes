08d11b7b22360e217d3cee89fbcb78f325585a60
Nivedita Pritam <166897201+niveditaPritam@users.noreply.github.com>
2025-11-12 19:25:45 +0530

    Minimum Number of Operations to Make All Array Elements Equal to 1.java

---
+++ Minimum Number of Operations to Make All Array Elements Equal to 1.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int minOperations(int[] nums) {
+        int n = nums.length;
+        int ones = 0;
+        
+        for (int num : nums) {
+            if (num == 1) ones++;
+        }
+
+        if (ones > 0) return n - ones;
+
+        int minLen = Integer.MAX_VALUE;
+        for (int i = 0; i < n; i++) {
+            int g = nums[i];
+            for (int j = i; j < n; j++) {
+                g = gcd(g, nums[j]);
+                if (g == 1) {
+                    minLen = Math.min(minLen, j - i + 1);
+                    break;
+                }
+            }
+        }
+
+        if (minLen == Integer.MAX_VALUE) return -1;
+
+        return (minLen - 1) + (n - 1);
+    }
+
+    private int gcd(int a, int b) {
+        return b == 0 ? a : gcd(b, a % b);
+    }
+}
