8f922cb8b3bc050e53296e3b4ddeb190ad1c1b1e
Nivedita Pritam <166897201+niveditaPritam@users.noreply.github.com>
2025-12-15 15:04:14 +0530

    Number of Smooth Descent Periods of a Stock.java

---
+++ Number of Smooth Descent Periods of a Stock.java
@@ -0,0 +1,16 @@
+class Solution {
+    public long getDescentPeriods(int[] prices) {
+        long ans = 0;
+        long count = 1;
+
+        for (int i = 0; i < prices.length; i++) {
+            if (i > 0 && prices[i] == prices[i - 1] - 1) {
+                count++;
+            } else {
+                count = 1;
+            }
+            ans += count;
+        }
+        return ans;    
+    }
+}
