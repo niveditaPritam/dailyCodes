0834f7827851e5635eb09a2774948ad6d1e5f264
Nivedita Pritam <166897201+niveditaPritam@users.noreply.github.com>
2025-12-25 13:38:05 +0530

    Maximize Happiness of Selected Children.java

---
+++ Maximize Happiness of Selected Children.java
@@ -0,0 +1,16 @@
+class Solution {
+    public long maximumHappinessSum(int[] happiness, int k) {
+        Arrays.sort(happiness);
+
+        long sum = 0;
+        int n = happiness.length;
+
+        for (int i = 0; i < k; i++) {
+            int current = happiness[n - 1- i] - i;
+            if (current > 0) {
+                sum += current;
+            }
+        }
+        return sum;    
+    }
+}
