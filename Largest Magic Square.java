96c7b477d89e6ab75b2bf70b599ad3c69a404864
Nivedita Pritam <166897201+niveditaPritam@users.noreply.github.com>
2026-01-18 17:20:45 +0530

    Largest Magic Square.java

---
+++ Largest Magic Square.java
@@ -0,0 +1,56 @@
+class Solution {
+    public int largestMagicSquare(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        int[][] row = new int[m][n + 1];
+        int[][] col = new int[m + 1][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                row[i][j + 1] = row[i][j] + grid[i][j];
+                col[i + 1][j] = col[i][j] + grid[i][j];
+            }
+        }
+
+        int maxSize = Math.min(m, n);
+
+        for (int k = maxSize; k >= 2; k--) {
+            for (int i = 0; i + k <= m; i++) {
+                for (int j = 0; j + k <= n; j++) {
+
+                    int target = row[i][j + k] - row[i][j];
+                    boolean isMagic = true;
+
+                    for (int r = i; r < i + k; r++) {
+                        if (row[r][j + k] - row[r][j] != target) {
+                            isMagic = false;
+                            break;
+                        }
+                    }
+
+                    for (int c = j; c < j + k && isMagic; c++) {
+                        if (col[i + k][c] - col[i][c] != target) {
+                            isMagic = false;
+                            break;
+                        }
+                    }
+
+                    if (isMagic) {
+                        int d1 = 0, d2 = 0;
+                        for (int x = 0; x < k; x++) {
+                            d1 += grid[i + x][j + x];
+                            d2 += grid[i + x][j + k - 1 - x];
+                        }
+                        if (d1 != target || d2 != target) {
+                            isMagic = false;
+                        }
+                    }
+
+                    if (isMagic) return k;
+                }
+            }
+        }
+        return 1; 
+    }
+}
