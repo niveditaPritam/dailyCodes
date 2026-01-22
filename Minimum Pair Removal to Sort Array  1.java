2affc986ebf66b667166f56f2b89a73c9a2f330e
Nivedita Pritam <166897201+niveditaPritam@users.noreply.github.com>
2026-01-22 17:50:45 +0530

    Minimum Pair Removal to Sort Array  1.java

---
+++ Minimum Pair Removal to Sort Array  1.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minimumPairRemoval(int[] nums) {
+        int count = 0;
+        
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] < nums[i - 1]) {
+                count++;
+            }
+        }
+        
+        return count;
+    }
+}
