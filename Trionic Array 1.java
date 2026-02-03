73dcab3e15b319ac0fb743655aed43298d949364
Nivedita Pritam <166897201+niveditaPritam@users.noreply.github.com>
2026-02-03 11:43:37 +0530

    Trionic Array 1.java

---
+++ Trionic Array 1.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isTrionic(int[] nums) {
+        int n = nums.length;
+        if (n < 3) return false;
+
+        int i = 1;
+
+        while (i < n && nums[i] > nums[i - 1]) {
+            i++;
+        }
+        if (i == 1 || i == n) return false;
+    
+    while (i < n&&nums[i] < nums[i - 1]) {
+        i++;
+    }
+    if (i == n) return false;
+
+    while (i < n&&nums[i] > nums[i-1]){
+        i++;
+    }
+
+    return i ==n;
+  }
+}
