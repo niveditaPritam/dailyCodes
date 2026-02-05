2475508137120d74c9ba5f31f06000de83962b77
Nivedita Pritam <166897201+niveditaPritam@users.noreply.github.com>
2026-02-05 21:24:24 +0530

    Transformed Array.java

---
+++ Transformed Array.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] constructTransformedArray(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > 0) {
+                int newIndex = (i + nums[i]) % n;
+                result[i] = nums[newIndex];
+            } else if (nums[i] < 0) {
+                int newIndex = (i + nums[i]) % n;
+                if (newIndex < 0) {
+                    newIndex += n;
+                }
+                result[i] = nums[newIndex];
+            } else {
+                result[i] = nums[i];
+            }
+        }
+
+        return result;
+    }
+}
