99355960c720516ab9bc6badb9a9d645a7f09753
Nivedita Pritam <166897201+niveditaPritam@users.noreply.github.com>
2026-02-07 11:41:47 +0530

    Minimum Deletions to Make String Balanced.java
    Implement method to calculate minimum deletions to balance string.

---
+++ Minimum Deletions to Make String Balanced.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minimumDeletions(String s) {
+        int countB = 0;
+        int deletions = 0;
+
+        for (char c : s.toCharArray()) {
+            if (c == 'b') {
+                countB++;
+            } else {
+                deletions = Math.min(deletions + 1, countB);
+            }
+
+        }
+        return deletions;    
+    }
+}
