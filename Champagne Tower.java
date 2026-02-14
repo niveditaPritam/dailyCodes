e8e21f8f1ef6510a5630a21f418d3864b87d71ec
Nivedita Pritam <166897201+niveditaPritam@users.noreply.github.com>
2026-02-14 16:46:25 +0530

    Champagne Tower.java

---
+++ Champagne Tower.java
@@ -0,0 +1,23 @@
+class Solution {
+    public double champagneTower(int poured, int query_row, int query_glass) {
+        
+        double[][] dp = new double[101][101];
+        dp[0][0] = poured;
+        
+        for (int row = 0; row < 100; row++) {
+            for (int col = 0; col <= row; col++) {
+                
+                if (dp[row][col] > 1) {
+                    double excess = (dp[row][col] - 1) / 2.0;
+                    
+                    dp[row + 1][col] += excess;
+                    dp[row + 1][col + 1] += excess;
+                    
+                    dp[row][col] = 1; 
+                }
+            }
+        }
+        
+        return Math.min(1, dp[query_row][query_glass]);
+    }
+}
