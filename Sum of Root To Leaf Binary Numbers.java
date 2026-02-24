ff29a4dd43b4dbb5e763e14fa662194ad31870db
Nivedita Pritam <166897201+niveditaPritam@users.noreply.github.com>
2026-02-24 21:56:13 +0530

    Sum of Root To Leaf Binary Numbers.java

---
+++ Sum of Root To Leaf Binary Numbers.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int sumRootToLeaf(TreeNode root) {
+        return dfs(root, 0);
+    }
+    
+    private int dfs(TreeNode node, int currentSum) {
+        if (node == null) {
+            return 0;
+        }
+        
+        currentSum = currentSum * 2 + node.val;
+        
+        if (node.left == null && node.right == null) {
+            return currentSum;
+        }
+        
+        return dfs(node.left, currentSum) + 
+               dfs(node.right, currentSum);
+    }
+}
